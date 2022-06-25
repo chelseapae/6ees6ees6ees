5df1078354bce1892629c2703530d0b7d430c910
Chelsea Pae <chelseapae@gmail.com>
2022-06-25 14:33:42 -0700

    Complete ForagerBee

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
+  constructor(age, job, color, food, canFly, treasureChest) {
+    super(age, job, color, food, canFly, treasureChest)
+    this.age = 10;
+    this.job = 'find pollen';
+    this.color = 'yellow';
+    this.food;
+    this.canFly = true;
+    this.treasureChest = [];
+  }
+
+  forage(treasure) {
+    this.treasureChest.push(treasure);
+  }
 };
