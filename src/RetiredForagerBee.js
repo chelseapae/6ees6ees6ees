7e0910b9e466b51bb750f08f59174ff743c063de
Chelsea Pae <chelseapae@gmail.com>
2022-06-25 14:36:59 -0700

    Complete Bare Minimum Requirements

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,18 @@
-class RetiredForagerBee {
-  // TODO..
+class RetiredForagerBee extends ForagerBee {
+  constructor(age, job, canFly, color, food, treasureChest){
+    super(age, job, canFly, color, food, treasureChest)
+    this.age = 40;
+    this.job = 'gamble';
+    this.canFly = false;
+    this.color = 'grey';
+    this.food;
+    this.treasureChest = [];
+  }
+  forage(){
+    return 'I am too old, let me play cards instead';
+  }
+
+  gamble(treasure){
+    this.treasureChest.push(treasure);
+  }
 };
