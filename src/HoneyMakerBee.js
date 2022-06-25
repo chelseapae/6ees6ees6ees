535474ba0f411a79fdf59b2cc7a4a0009acf9bab
Chelsea Pae <chelseapae@gmail.com>
2022-06-25 13:30:32 -0700

    Complete HoneyMakerBee

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,17 @@
-class HoneyMakerBee {
-  // TODO..
+class HoneyMakerBee extends Bee {
+  constructor(age, job, color, food, eat, honeyPot) {
+    super(age, job, color, food, eat, honeyPot);
+    this.age = 10;
+    this.job = 'make honey';
+    this.color = 'yellow';
+    this.food;
+    this.eat;
+    this.honeyPot = 0;
+  }
+  makeHoney(){
+    this.honeyPot++;
+  }
+  giveHoney(){
+    this.honeyPot--;
+  }
 };
