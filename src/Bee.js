75f373569343c14eacab9490a3ca7f08b8481c64
Chelsea Pae <chelseapae@gmail.com>
2022-06-25 13:26:05 -0700

    Complete Bee

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,10 @@
-class Bee {
-  // TODO..
+class Bee extends Grub {
+    constructor(age, color, food, job){
+    super(age, color, food, job);
+    this.age = 5;
+    this.color = 'yellow';
+    this.food;
+    this.job = 'Keep on growing'
+  }
+
 };
